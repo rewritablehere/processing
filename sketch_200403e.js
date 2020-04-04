ede0fa87f2d8164c2c5aaef747806d50b4497b55
rewritablehere <ananthacp@gmail.com>
2020-04-05 02:34:57 +0530

    b

--- sketch_200403e.js
+++ sketch_200403e.js
@@ -9,17 +9,32 @@
  */
 let player, playerleft, playerright, playerup, playerdown, x, y, prevx, prevy; // Declare variable 'img'.
 
-let img1, img2;
-let moveby = 32, gx, gy;
+var img = [];
+var gx = [];
+var gy = [];
+let moveby = 32;
 let adjx = 0, adjy = -13;
 let groundx = 32, groundy = 32;
+var groundtype = 0;
 
 function setup() {
   createCanvas(1440,813);
   
-  
-  img1 = loadImage('assets/ground1.png');
-  img2 = loadImage('assets/ground2.png');
+  for (let i = 0; i < 30; i++) {
+      if (groundtype = 0) {
+      img[i] = loadImage('assets/ground1.png');
+      groundtype = 1;
+      } else {
+      img[i] = loadImage('assets/ground2.png');
+      groundtype = 0;
+      } 
+
+    gx[i] = groundx*random(44);
+    gy[i] = groundy*random(24);
+    
+    
+  }
+
   playerleft = loadImage('assets/left.png');
   playerright = loadImage('assets/right.png');
   playerup = loadImage('assets/up.png');
@@ -27,16 +42,15 @@ function setup() {
   background('#F5F5F7');
   x = width/3; y = height/3;
   player = playerright;
-  gx1 = width/3;
-  gy1 = height/3;
-  gx2 = width/3 + groundx + adjx;
-  gy2 = height/3 + groundy + adjy;  
+ 
 }
 
 function draw() {
   background('#F5F5F7');
-  image(img1, gx1, gy1);
-  image(img2, gx2, gy2);
+  for (let i = 0; i < (img.length-1); i++) {
+  image(img[i], gx[i], gy[i]);
+  }
+  
   image(player, x, y);
   prevx = x;
   prevy = y;
