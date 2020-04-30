c72be6d36ee9aace16d29e2f913391644e78c180
rewritablehere <ananthacp@gmail.com>
2020-05-01 01:21:09 +0530

    Update sketch_200403e.js

--- sketch_200403e.js
+++ sketch_200403e.js
@@ -1,86 +1,91 @@
-let player, playerleft, playerright, playerup, playerdown; 
+let playerleft, playerright, playerup, playerdown, px, py;
+let left = 'left', right = 'right', up = 'up', down = 'down';
+var groundimg = [];
 
-var img = [];
-var gx = [];
-var gy = [];
-let right = 1, left = 2, up = 3, down = 4;
 
-var playr = {
-x:480,
-y:271,
-px:480,
-py:271,
-moveby:32,
-turnleft : function() {
-  player = playerleft;
-},
-turnright : function() {
-  player = playerright;
-},
-turnup : function() {
-  player = playerup;
-},
-turndown : function() {
-  player = playerdown;
-},
+var player = {
+    x:64,
+    y:64,
+    size:64, 
+    
+    
+    display: function(direction) {
+      
+    px = this.x;
+    py = this.y;
+    
+       switch (direction) {
+       case 'left':
+            this.x = px - this.size;
+            image(playerleft, this.x, this.y); 
+            break;
+       case 'right':
+            this.x = px + this.size;
+            image(playerright, this.x, this.y); 
+            break;
+       case 'up':
+            this.y = py - this.size;
+            image(playerup, this.x, this.y); 
+            break;
+       case 'down':
+            this.y = py + this.size;
+            image(playerdown, this.x, this.y); 
+            break;
+       }
+          
+    },
 };
 
 var ground = {
-w: 32,
-h: 32,
-type: 0,
+    w: 64,
+    h: 64,
+    
+    display: function() {
+      for (let j = 0; j < 10; j++) {
+      for (let k = 0; k < 10; k++) {
+      image(groundimg[k], (j*this.w), (k*this.h));
+      } 
+      }
+    },
 };
 
-
-
 function setup() {
-  createCanvas(1440,813);
-  
-  for (let i = 0; i < 30; i++) {
 
-      img[i] = loadImage('assets/ground1.png');
+  createCanvas(1440,788);
 
-    gx[i] = ground.w*random(44);
-    gy[i] = ground.h*random(24);
-    
-    
-  }
+  for (let i = 0; i < 220; i++) {
+  groundimg[i] = loadImage('assets/ground1.png');
+  } 
 
   playerleft = loadImage('assets/left.png');
   playerright = loadImage('assets/right.png');
   playerup = loadImage('assets/up.png');
   playerdown = loadImage('assets/down.png');
-  background('#F5F5F7');
-  playr.turnright();
+
+  //background('#F5F5F7');
+  //ground.display();
+  //player.display();
 }
 
 function draw() {
-  background('#F5F5F7');
-  for (let i = 0; i < (img.length-1); i++) {
-  image(img[i], gx[i], gy[i]);
-  }
-  
-  image(player, playr.x, playr.y);
-  playr.px = playr.x;
-  playr.py = playr.y;
 
 }
 
 function keyPressed() {
-  if (keyCode === LEFT_ARROW) {
-    playr.x = playr.px - playr.moveby;
-    playr.turnleft();
-    
-  } else if (keyCode === RIGHT_ARROW) {
-    playr.x = playr.px + playr.moveby;
-    playr.turnright();
+  if (keyCode == LEFT_ARROW) {
 
-  } else if (keyCode === UP_ARROW) {
-    playr.y = playr.py - playr.moveby;
-    playr.turnup();
+    player.display(left);
     
-  } else if (keyCode === DOWN_ARROW) {
-    playr.y = playr.y + playr.moveby;
-    playr.turndown();
+  } else if (keyCode == RIGHT_ARROW) {
+    
+    player.display(right);
+
+  } else if (keyCode == UP_ARROW) {
+    
+    player.display(up);
+    
+  } else if (keyCode == DOWN_ARROW) {
+    
+    player.display(down);
   } 
 }
